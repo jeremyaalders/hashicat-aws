11365ca2c70e7feb64fa1e24496e7108c8d328e3
Jeremy Aalders <jeremy.aalders@resmed.com>
2020-09-17 19:42:26 +0000

    Add VPC file take 2

--- files/vpc.tf
+++ files/vpc.tf
@@ -1,5 +1,5 @@
 module "vpc" {
   source  = "app.terraform.io/JEREMY-training/vpc/aws"
   version = "2.51.0"
-  # insert required variables here
+  # insert required variables here make more deltas
 }
